4b17063174b5dc12d3110cdd351377d7279a12c8
Sherah <sherah@sherah.org>
2013-02-23 11:19:30 -0800

    set up functions, WIP structuring app with new emberjs knowledges

--- app.js
+++ app.js
@@ -1,1 +1,67 @@
 window.App = Ember.Application.create();
+
+App.Card = Em.Object.extend({
+	question: "this is a question",
+	answer: "this is an answer"
+});
+
+App.cardController = Em.ArrayProxy.create({
+	content: [];
+
+	createQuestion: function(){
+
+	},
+
+	createAnswer: function(){
+
+	},
+
+	flipCard: function(){
+
+	},
+
+	deleteCard: function(){
+
+	},
+
+	skipCard: function(){
+
+	},
+
+	answerCard: function(){
+
+	},
+
+	shuffleCards: function(){
+
+	},
+
+	resetCards: function(){
+
+	},
+
+	addNewDeck: function(){
+
+	},
+
+	displayDeck: function(){
+		
+	},
+
+});
+
+var mainView = Ember.View.create({
+	templateName = 'mainView'
+});
+
+var sideView = Ember.View.create({
+	templateName = 'sideView'
+});
+
+var cardView = Ember.View.create({
+	templateName = 'cardView'
+});
+
+var deckView = Ember.View.create({
+	templateName = 'deckView'
+});
